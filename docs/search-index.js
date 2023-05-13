e57428c201076f08cc429c7f8c551ab5e3668763
CI <you@example.com>
2023-05-13 10:54:12 +0000

    Update docs

---
+++ docs/search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = JSON.parse('{\
+"qwe":{"doc":"","t":"F","n":["add"],"q":["qwe"],"d":[""],"i":[0],"f":[[[1,1],1]],"p":[[15,"usize"]]}\
+}');
+if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
+if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
